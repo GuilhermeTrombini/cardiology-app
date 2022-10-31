4afc025ced0cea6d2e6f2cabc5e9b7678a497744
Guilherme <guilherme.trombini@labsit.io>
2022-10-31 14:52:37 -0300

    feat: added native configuration to project structure

---
+++ screens/Home/HomeScreen.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  useColorScheme,
+  View,
+  Text,
+} from 'react-native';
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+import {} from './styles';
+
+const HomeScreen = ({navigation}) => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Text onPress={() => navigation.navigate('ARScreen')}>
+            Navigate to ARScreen
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
